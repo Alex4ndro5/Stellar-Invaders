a8380420ec91883ca46b4117deab6a0c1c9fb439
alekfolfas <alekfolfas@student.agh.edu.pl>
2022-12-30 17:16:31 +0000

    Base

--- GameSI.cs
+++ GameSI.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Stellar_Invaders
 {
-    public class Game1 : Game
+    public class GameSI : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        public GameSI()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿
-using var game = new Stellar_Invaders.Game1();
+using var game = new Stellar_Invaders.GameSI();
 game.Run();
